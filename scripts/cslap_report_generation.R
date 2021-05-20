3a16bdfde799076f561d1ffe0f1cfcef7bdb9f37
Annalee Tweitmann <Annaleetweitmann@gmail.com>
2021-05-20 12:09:35 -0400

    adding data necessary to populate reports, edited script to change format to tabs, changed seasonal/longterms means to median, etc.

--- scripts/cslap_report_generation.R
+++ scripts/cslap_report_generation.R
@@ -21,58 +21,17 @@ library(kableExtra)
 library(magick)
 library(lubridate)
 library(BBmisc)
+library(gt)
 
 ###Read in data
-setwd("C:/Users/amtweitm/OneDrive - New York State Office of Information Technology Services/Documents/R/Current")
-lake<-read.csv("from.other.files/lake.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-class<-read.csv("from.other.files/Lake.Master.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-class<-class %>% rename(LAKE_ID=LakeID) %>% select(LAKE_ID,PWLID,Waterbody_Classification,M_BAS_NAME,M_BASIN_ID,BasinSub,ACRES,AREA,LEN) %>% distinct() 
-lake<-merge(lake,class,by=c('LAKE_ID'),all.x=TRUE)
-rm(class)
+setwd("C:/Users/amtweitm/OneDrive - New York State Office of Information Technology Services/Documents/R/CSLAP_reporting/data")
 
-county<-read.csv("from.other.files/Location-Clean-10-29-2019.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-county<-county %>% rename(LAKE_ID=LakeID,LOCATION_ID=LocationID) %>% select(LOCATION_ID,County,DEC.Region) %>% distinct()
-location<-read.csv("from.other.files/location.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-location<-merge(location,county,by="LOCATION_ID",all.x=TRUE)
-rm(county)
-
-profiles<-read.csv("DEPTH.PROFILE.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-results<-read.csv("Test.Results.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-sample<-read.csv("Sample.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-sample<-sample %>% mutate(LOCATION_ID=gsub("\\n","",LOCATION_ID),LAKE_ID=gsub("\\n","",LAKE_ID))
-habs<-read.csv("HABstatus.csv",na.strings=c("","NA"), stringsAsFactors=FALSE)
-
-#Merge data tables
-data<-merge(sample,results,by=c('SAMPLE_ID','SAMPLE_NAME','INFO_TYPE'),all.x=TRUE)
-data<-data %>% filter(!is.na(Characteristic.Name))
-data2<-merge(sample,profiles,by=c("SAMPLE_ID",'INFO_TYPE'),all.x=TRUE)
-data2<-data2 %>% filter(!is.na(Characteristic.Name))
-data<-merge(data,data2,by=c("LAKE_ID","LOCATION_ID","SAMPLE_ID","SAMPLE_NAME","DATA_PROVIDER","INFO_TYPE","SAMPLE_DATE",
-                            "TIME","START_DEPTH","END_DEPTH","ESF.","UFI.","REMARK","BLOOM_LOC",
-                            "WIND_DIR","WIND_INT","EXTENT","BLOOM_DESC","EQUIPMENT_DESC","EQUIPMENT_TYPE","LAKE_PROJ_CODE",
-                            "SAMPLE_METHOD","PERCENT","DEPTH_UNIT","Characteristic.Name","Result.Value","Result.Unit"),all=TRUE)
-rm(data2)
-data<-merge(data,sample,by=c("LAKE_ID","LOCATION_ID","SAMPLE_ID","SAMPLE_NAME","DATA_PROVIDER","INFO_TYPE","SAMPLE_DATE",
-                             "TIME","START_DEPTH","END_DEPTH","ESF.","UFI.","REMARK","BLOOM_LOC",
-                             "WIND_DIR","WIND_INT","EXTENT","BLOOM_DESC","EQUIPMENT_DESC","EQUIPMENT_TYPE","LAKE_PROJ_CODE",
-                             "SAMPLE_METHOD","PERCENT","DEPTH_UNIT"),all=TRUE)
-data<-merge(data,lake,by=c('LAKE_ID'),all=TRUE)
-data<-merge(data,location,by=c('LOCATION_ID','LAKE_ID'),all = TRUE)
-data<-merge(data,habs, by=c('SAMPLE_ID'),all.x = TRUE)
-data<-data %>%
-  mutate(SAMPLE_DATE=as.Date(SAMPLE_DATE,format="%Y-%m-%d")) %>% 
-  filter(INFO_TYPE!="MP") %>% 
-  filter(!is.na(LAKE_ID))
-
-#remove individual data tables in working environment
-rm(list = c('lake','profiles','location','results','sample','habs'))
+source("old_database/Reading.LMAS.Data.R")
 
 #Make sure records are distinct
 data<-data %>% distinct()
 setwd("C:/Users/amtweitm/OneDrive - New York State Office of Information Technology Services/Documents/R/CSLAP_reporting")
 lake<-data
-lake <- lake %>% 
-  filter(DATA_PROVIDER == "CSL")
 lake$SAMPLE_DATE<-as.Date(lake$SAMPLE_DATE,format="%Y-%m-%d")
 lake<-lake %>% 
   mutate(year=substring(SAMPLE_DATE,1,4))
@@ -82,23 +41,79 @@ lake<-lake %>%
          WATER,LocationName,Y_Coordinate,X_Coordinate,INFO_TYPE,Waterbody_Classification,PWLID, LAB_NAME_CODE,
          year,ACRES,County,PWS,Beaches,REMARK,STATUS,QUANTITATION_LIMIT,VALIDATOR_QUALIFIERS,INTERPRETED_QUALIFIERS)
 lake<-lake[!is.na(lake$SAMPLE_ID),]
-lakes<-unique(lake[c('LAKE_ID','WATER')])
-lakes<-lakes[!is.na(lakes$LAKE_ID),]
+# lakes<-unique(lake[c('LAKE_ID','WATER')])
+# lakes<-lakes[!is.na(lakes$LAKE_ID),]
+
+lakes_onesite <- read.csv("C:/Users/amtweitm/OneDrive - New York State Office of Information Technology Services/Documents/R/CSLAP_reporting/data/one_site_lake_list_2020.csv")
+lakes_multisites <- read.csv("C:/Users/amtweitm/OneDrive - New York State Office of Information Technology Services/Documents/R/CSLAP_reporting/data/multi_site_lake_list_2020.csv")
+silver_lake <- read.csv("C:/Users/amtweitm/OneDrive - New York State Office of Information Technology Services/Documents/R/CSLAP_reporting/data/silver_lake.csv")
 
 ###For loop that runs RMarkdown for every lake with CSLAP data
 ###Change output directory to folder for year
+###change report year
 
-nlakes <- nrow(lakes)
-for(i in 1:nlakes){
-  report_year <- "2018" 
+
+report_year <- "2020" 
+
+#nlakes <- nrow(lakes)
+for(i in 1:nrow(lakes_onesite)){
+  habs_lake1 <- lake %>% 
+    filter(LAKE_ID == lakes_onesite[i,2])
   lake1<-lake %>% 
-    filter(LAKE_ID == lakes[i,1])
+    filter(LAKE_ID == lakes_onesite[i,2]) %>% 
+    filter(DATA_PROVIDER == "CSL")
   titles<-lake1$WATER[1]
   ids<-lake1$LAKE_ID[1]
+  multisite_var <- 0 
   rmarkdown::render('scripts/cslap_report_markdown.Rmd',
                     output_file =  paste(report_year, "_", "report_", titles,"(",ids,")", ".html", sep=''), 
-                    output_dir = 'reports/')
+                    output_dir = 'reports/2020')
 }
+
+for(i in 1:nrow(lakes_multisites)){
+  habs_lake1 <- lake %>% 
+    filter(LAKE_ID == lakes_onesite[i,2])
+  lake1<-lake %>% 
+    filter(LAKE_ID == lakes_onesite[i,2]) %>% 
+    filter(DATA_PROVIDER == "CSL") %>% 
+    filter(LOCATION_ID == lakes_onesite[i,1])
+  titles<-lakes_multisites$NAME[i]
+  ids<-lakes_multisites$LAKE_ID[i]
+  loc_id <- lakes_multisites$LOCATION_ID[i]
+  multisite_var <- 1
+  rmarkdown::render('scripts/cslap_report_markdown.Rmd',
+                    output_file =  paste(report_year, "_", "report_", titles,"(",ids,")", ".html", sep=''), 
+                    output_dir = 'reports/2020')
+}
+
+for(i in 1:nrow(silver_lake)){
+habs_lake1 <- lake %>% 
+  dplyr::filter(year < "2020") %>% 
+  dplyr::filter(LAKE_ID == silver_lake$Lake_ID[i])
+  
+if(silver_lake$Lake_ID[i] == "0904SIL0351"){
+  list_of_2020_samps <- c("20-117-01", "20-117-02", "20-117-03", "20-117-04", "20-117-05", "20-117-06", "20-117-07" )
+} else if (silver_lake$Lake_ID[i] == "0403SIL0115"){
+  list_of_2020_samps <- c("20-25-01", "20-25-02", "20-25-03", "20-25-04", "20-25-05", "20-25-06", "20-25-07", "20-25-08", 
+                          "20-25-11", "20-25-12", "20-25-13", "20-25-14", "20-25-15", "20-25-16", "20-25-17", "20-25-18")
+}
+  
+lake1 <- lake %>% 
+  dplyr::filter(DATA_PROVIDER == "CSL") %>% 
+  dplyr::filter(SAMPLE_NAME %in% list_of_2020_samps | LAKE_ID == silver_lake$Lake_ID[1] & year < as.numeric("2020")) 
+
+titles<- "Silver Lake"
+ids<- silver_lake$Lake_ID[i]
+multisite_var <- 0 
+rmarkdown::render('scripts/cslap_report_markdown.Rmd',
+                  output_file =  paste(report_year, "_", "report_", titles,"(",ids,")", ".html", sep=''), 
+                  output_dir = 'reports/2020')
+
+}
+
+
+
+
 
 
 
